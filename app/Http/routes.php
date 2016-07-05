163e64d265b24f29d921c5cf119a92983a5538ca
Manuel <manuellm@ymail.com>
2016-07-05 17:12:34 -0500

    Agregue ruta menu

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,3 +19,8 @@ Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
+
+Route::get('menu', function(){
+    return 'Hola Mundo';
+});
+
